4845c6d63e93d0b777e3c8736d63a969ad50b845
diclelvira <diclehatman3@gmail.com>
2025-03-08 15:46:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Menü açma ve kapama işlevi
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('show'); // Menüye "show" sınıfını ekleyip çıkararak menüyü açar ve kapatır
+}
+
